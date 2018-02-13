29e91b50cdc0ee0a3423bddca788115fba5e64e5
Artyom <as.fedotkin@yandex.ru>
2018-02-13 18:40:07 +0300

    добавлена тестовая страница test.html

---
+++ res/js/bvi-init-panel.js
@@ -0,0 +1,16 @@
+/*!
+ * Button visually impaired v1.0.6
+ */
+ jQuery(document).ready(function($) {
+ $('.bvi-panel-open').bvi('Init', {"BviPanel" : "1",
+                                   "BviPanelBg" : "white",
+                                   "BviPanelFontSize" : "12",
+                                   "BviPanelLetterSpacing" : "normal",
+                                   "BviPanelLineHeight" : "normal",
+                                   "BviPanelImg" : "1",
+                                   "BviPanelImgXY" : "1",
+                                   "BviPanelReload" : "0",
+                                   "BviCloseClassAndId" : ".hide-screen-fixed",
+                                   "BviFixPanel" : "1",
+                                   "BviPlay" : "1"}); 
+ });
